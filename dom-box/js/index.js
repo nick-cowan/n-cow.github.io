cf9f0050b8fe2f547e69379815fc8e5df848fcfd
nick-cowan <nickcowan@NickBook.home>
2017-08-04 10:07:18 +0100

    change to mp3

--- dom-box/js/index.js
+++ dom-box/js/index.js
@@ -1,30 +1,30 @@
 $(document).ready(function() {
 
-  var padOne = new Audio('./sounds/bird.mov');
+  var padOne = new Audio('./sounds/bird.mp3');
   $('.pad-1').mousedown(function() {
     padOne.load()
     padOne.play();
   });
 
-  var padTwo = new Audio('./sounds/butter.mov');
+  var padTwo = new Audio('./sounds/butter.mp3');
   $('.pad-2').mousedown(function() {
     padTwo.load();
     padTwo.play();
   });
 
-  var padThree = new Audio('./sounds/revel.mov');
+  var padThree = new Audio('./sounds/revel.mp3');
   $('.pad-3').mousedown(function() {
     padThree.load();
     padThree.play();
   });
 
-  var padFour = new Audio('./sounds/sure.mov');
+  var padFour = new Audio('./sounds/sure.mp3');
   $('.pad-4').mousedown(function() {
     padFour.load();
     padFour.play();
   });
 
-  var padFive = new Audio('./sounds/taffy.mov');
+  var padFive = new Audio('./sounds/taffy.mp3');
   $('.pad-5').mousedown(function() {
     padFive.load();
     padFive.play();
